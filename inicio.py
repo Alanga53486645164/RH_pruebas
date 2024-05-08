f2b897b90ad38947b54d99bf95009ee5b6b8d3c7
ian <liquit5.is@outlook.com>
2024-05-08 08:04:29 -0600

    Pagina404

--- inicio.py
+++ inicio.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('Notfound.html'), 404
+
 @app.route('/area')
 def area():
     conn = pymysql.connect(host='localhost', user='root', passwd='', db='rh3' )
