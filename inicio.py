1e6947b44da40251886323ab441efd2bb042f650
Martin <22301061550038@cetis155.edu.mx>
2024-05-13 21:35:11 -0600

    pagina inxistente

--- inicio.py
+++ inicio.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html",inicio=True)
 
-# @app.errorhandler(404)
-# def page_not_found(error):
-#     return render_template('Notfound.html'), 404
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('Notfound.html'), 404
 
 @app.route('/catalogosEdita:<string:tabla>:<int:id_campo>')
 def editar(tabla,id_campo):
