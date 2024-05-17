f992da6e57e005adf5293fa60bc2e0c1cdfcaff8
Martin <22301061550038@cetis155.edu.mx>
2024-05-17 12:29:46 -0600

    bd actualizada

--- cursos.sql
+++ cursos.sql
@@ -1,11 +1,13 @@
 use rh3;
 set sql_safe_updates=0;
-drop table empleados;
-drop table cursos;
-drop table puesto_has_cursos;
-drop table curso_has_empleados;
-drop table modo_aplicacion_curso;
-drop table curso_has_aparicion;
+drop table if exists curso_has_aparicion;
+drop table if exists puesto_has_cursos;
+drop table if exists curso_has_empleados;
+drop table if exists curso;
+drop table if exists cursos;
+drop table if exists empleados;
+drop table if exists modo_aplicacion_curso;
+
 
 /*
 create table dias(
@@ -23,8 +25,8 @@ insert into dias(dia) values
 */
 /*Provicional en lo que conseguimos la tabla del otro equipo*/
 create table empleados(
-	id_empleado int primary key auto_increment,
-    nombre varchar(40)
+	id_empleado int primary key auto_increment not null,
+    nombre varchar(40) not null
     #...Equipo colaborando
 );
 insert into empleados(nombre) values
@@ -35,12 +37,12 @@ insert into empleados(nombre) values
     ("JUAN");
 /**/
 create table cursos(
-	id_curso int primary key auto_increment,
-    nombre varchar(50),
-    descripcion varchar(100),
-    duracion varchar (15), 
-    objetivos_de_aprendizaje varchar(300),
-    obligatorio boolean
+	id_curso int primary key auto_increment not null,
+    nombre varchar(50) not null,
+    descripcion varchar(100) not null,
+    duracion varchar (15) not null, 
+    objetivos_de_aprendizaje varchar(300) not null,
+    obligatorio boolean not null
 );
 insert into cursos(nombre,descripcion,duracion,objetivos_de_aprendizaje,obligatorio) values
 	("Curso de Induccion","Curso para la capacitacion de nuevos empleados","32 horas","entendimiento de politicas de la empresa, desempeño optimo del trabajo",True),
@@ -69,47 +71,48 @@ create table curso_has_horario (
 );
 */
 create table puesto_has_cursos(
-	id_registro int primary key auto_increment,
-    id_curso int,
-    id_puesto int
+	id_registro int primary key auto_increment not null,
+    id_curso int not null,
+    id_puesto int not null
+    #,foreign key (id_curso) references cursos(id_curso),
+    #foreign key (id_puesto) references puesto(idPuesto)
 );
 insert into puesto_has_cursos(id_curso,id_puesto) values
 	(1,1),
     (1,3);
 create table curso_has_empleados(
-	id_registro int primary key auto_increment, 
-	id_curso int, 
-	id_empleado int , 
-	#inicio date, 
-	#fin date, 
+	id_registro int primary key auto_increment not null, 
+	id_curso int not null, 
+	id_empleado int not null, 
 	calificacion int
+    #,foreign key (id_curso) references cursos(id_curso),
+    #foreign key (id_empleado) references empleados(id_empleado)
 );
 insert into curso_has_empleados(id_curso,id_empleado,calificacion) values
 	/*(1,2,"2002-04-02","2002-04-20",null),
     (2,2,"2002-03-27","2002-04-15",8);*/
     (1,2,null),
-    (2,2,8);
-#delete from curso_has_empleados;
-#drop table curso_has_empleados;
-select * from curso_has_empleados;
+    (2,2,8); 
 
 create table modo_aplicacion_curso(
-	id_modo int primary key,
-    nombre varchar(20)
+	id_modo int primary key auto_increment not null,
+    nombre varchar(40) not null
 );
 insert into modo_aplicacion_curso(nombre) values
 	("PRESENCIAL"),
     ("SOLO MATERIAL DIDACTICO"),
     ("VIRTUAL");
 create table curso_has_aparicion(
-	id_registro int primary key auto_increment,
-    id_metodo_aplicacion int ,
-    lugar varchar(40),
-    id_curso int,
-    inicio date,
-    fin date,
-	/*? la dejo?*/
-    id_encargado int /*fk a empleados*/
+	id_registro int primary key auto_increment not null,
+    id_metodo_aplicacion int not null,
+    lugar varchar(40) not null,
+    id_curso int not null,
+    inicio date not null,
+    fin date not null,
+    id_encargado int not null
+    #,foreign key (id_curso) references cursos(id_curso),
+    #foreign key (id_metodo_aplicacion) references modo_aplicacion_curso(id_modo),
+    #foreign key (id_encargado) references empleados(id_empleado)
 );
 insert into curso_has_aparicion(id_metodo_aplicacion,lugar,id_curso,inicio,fin,id_encargado) values
 	(1,"Area 51",1,"2002-04-02","2002-04-20",2),
